a4a562b6f79e91c840925339be152ee60534fc6c
Narutayusu <kamondach.yusu@bumail.net>
2024-03-19 21:36:15 +0700

    finish script move

--- Assets/move.cs
+++ Assets/move.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class move : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float force, forceA , mass, acceleration;
+    private bool canJump = true;
+    //public float jumpCooldown = 1f;
+
+// Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+        Jump();
         
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Vector3.left * force);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Vector3.right * force);
+            
+        }
+    }
+
+    public Rigidbody rb;
+
+    void FixedUpdate()
+    {
+       
+        
+    }
+
+    void Jump()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Space)&& canJump)
+        {
+            forceA = mass * acceleration;
+            mass = GetComponent<Rigidbody>().mass;
+            GetComponent<Rigidbody>().AddForce(0,forceA,forceA);
+            canJump = false;
+            //StartCoroutine(JumpCooldown());
+        }
+    }
+    /*private IEnumerator JumpCooldown()
+    {
+        yield return new WaitForSeconds(jumpCooldown);
+        canJump = true;
+    }*/
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            canJump = true;
+        }
     }
 }
